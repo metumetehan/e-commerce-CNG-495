c0ade656c95faa8e1d88a0330a363e3e4585c04a
metumetehan <metumetehan@gmail.com>
2024-01-02 21:04:18 +0200

    detect_labels dunctionality is fully implemented
    object recognition implementation. full implementation of aws recognito and database updating is done.

--- server/objectRecognation/lambda_function.py
+++ server/objectRecognation/lambda_function.py
@@ -2,19 +2,29 @@ import json
 import urllib.parse
 import boto3
 
-s3 = boto3.client('s3')
-db = boto3.client('dynamodb')
+s3 = boto3.client('s3',region_name='eu-central-1')
+db = boto3.client('dynamodb',region_name='eu-central-1')
+rk = boto3.client('rekognition',region_name='eu-central-1')
 
 def lambda_handler(event, context):
 
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    print(bucket,key);
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-
-        """
+        result = rk.detect_labels(
+        Image={'S3Object':{'Bucket':bucket,'Name': key},},
+         MaxLabels=1,
+         Features=[
+        'GENERAL_LABELS',
+        ],
+         
+         )
+        print(result)
+        
+        
         update_response = db.update_item(
-            TableName= "item",
+            TableName= "item_table",
             Key={"item_ID": {
                 "S": key
                 }
@@ -24,15 +34,15 @@ def lambda_handler(event, context):
             },
             ExpressionAttributeValues={
                 ':c': {
-                    "S": "unknown",
+                    "S": result["Labels"][0]["Categories"][0]["Name"],
                 },
             },
             UpdateExpression='SET #C = :c',
             ReturnValues="UPDATED_NEW",
         )
-        """
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        
+        
+        return "Success"
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
